126812ac9ddedbd7b1175745182fee1733ec850d
lukasz-kowalski <lkowalski9090@gmail.com>
2020-10-21 21:46:51 +0200

    CRA cleanup

---
+++ default-theme.ts
@@ -0,0 +1,44 @@
+import { DefaultTheme } from 'styled-components'
+
+const defaultTheme: DefaultTheme = {
+  spacing: {
+    4: '4px',
+    8: '8px',
+    16: '16px',
+    24: '24px',
+    32: '32px',
+    40: '40px',
+    48: '48px',
+    56: '56px'
+  },
+
+  fontSize: {
+    xsmall: '10px',
+    small: '14px',
+    base: '16px',
+    large: '24px',
+    xlarge: '30px'
+  },
+
+  borderRadius: {
+    semiRounded: '16px',
+    rounded: '24px',
+    circle: '100px'
+  },
+
+  colors: {
+    purple60: '#4C00C2',
+    purple90: '#32007E',
+    purple: '#3B058E',
+    background: '#E5E5E5',
+    white: '#FFFFFF',
+    grey20: '#D3D8E1',
+    grey50: '#798291',
+    grey70: '#444E5D',
+    grey90: '#1A212C',
+    green30: '#19AC51',
+    red30: '#FC484C'
+  }
+}
+
+export { defaultTheme }
---
+++ styled.d.ts
@@ -0,0 +1,44 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    spacing: {
+      4: string;
+      8: string;
+      16: string;
+      24: string;
+      32: string;
+      40: string;
+      48: string;
+      56: string;
+    },
+
+    fontSize: {
+      xsmall: string;
+      small: string;
+      base: string;
+      large: string;
+      xlarge: string;
+    },
+
+    borderRadius: {
+      semiRounded: string;
+      rounded: string;
+      circle: string;
+    },
+
+    colors: {
+      purple60: string;
+      purple90: string;
+      purple: string;
+      background: string;
+      white: string;
+      grey20: string;
+      grey50: string;
+      grey70: string;
+      grey90: string;
+      green30: string;
+      red30: string;
+    }
+  }
+}
